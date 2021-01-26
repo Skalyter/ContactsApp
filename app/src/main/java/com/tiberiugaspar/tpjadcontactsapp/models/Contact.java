98cf57057ddf85ecbd4a706e05a532164c0bf981
Tiberiu Gaspar <tiberiu.gaspar98@gmail.com>
2021-01-26 10:27:43 +0200

    generated javadoc and added comments for models package

--- app/src/main/java/com/tiberiugaspar/tpjadcontactsapp/models/Contact.java
+++ app/src/main/java/com/tiberiugaspar/tpjadcontactsapp/models/Contact.java
@@ -3,6 +3,13 @@ package com.tiberiugaspar.tpjadcontactsapp.models;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The model class for any Contact instance
+ *
+ * <p>A contact must have a userId, a contactId, firstName and at least one phone number to be stored
+ * in the database.</p>
+ * <p>All the other fields are optional, therefore are treated accordingly.</p>
+ */
 public class Contact {
 
     private String userId;
@@ -40,8 +47,14 @@ public class Contact {
         this.firstName = firstName;
     }
 
+    /**
+     * To avoid a {@link NullPointerException}, if the lastName is null, we set it to the empty string
+     * value, for display purposes.
+     *
+     * @return the lastName if it is not null; an empty string otherwise.
+     */
     public String getLastName() {
-        if (lastName==null) lastName = "";
+        if (lastName == null) lastName = "";
         return lastName;
     }
 
--- app/src/main/java/com/tiberiugaspar/tpjadcontactsapp/models/PhoneNumber.java
+++ app/src/main/java/com/tiberiugaspar/tpjadcontactsapp/models/PhoneNumber.java
@@ -1,5 +1,20 @@
 package com.tiberiugaspar.tpjadcontactsapp.models;
 
+/**
+ * The model class for any PhoneNumber instance.
+ * <p>It is subordinated to the {@link Contact} class, as a phoneNumber must be assigned to a Contact
+ * object, and a contact object might have one or many phoneNumbers.</p>
+ * <p>It contains a string holding the actual phoneNumber and an integer value to store the category,
+ * as follows: case 0: "No label";
+ * case 1: "Mobile";
+ * case 2: "Home";
+ * case 3: "Work";
+ * case 4: "Main";
+ * case 5: "Work fax";
+ * case 6: "Home fax";
+ * default: "Others";
+ * </p>
+ */
 public class PhoneNumber {
     private String phoneNumber;
     private int category;
